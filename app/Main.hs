649b77d0c909895aa92d702a75e76db02342d06d
Asher Harvey-Smith <asher.harvey-smith@warwick.ac.uk>
2022-06-11 12:48:15 +0100

    Make all imports qualified

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,18 @@
 module Main where
 
-import Morse (wordToMorse)
-import Words (testWords)
+import qualified Morse (wordToMorse)
+import qualified Words (testWords)
 
 import qualified System.Random as Random (randomRIO)
-import Data.Vector (Vector)
+-- import Data.Vector (Vector)
 import qualified Data.Vector as Vector (unsafeIndex, length)
 import qualified Data.List as List (intersperse)
 
 getTest :: IO (String, String)
 getTest = do
-    r <- Random.randomRIO (0, Vector.length testWords - 1)
-    let w = Vector.unsafeIndex testWords r
-    return (w, wordToMorse w)
+    r <- Random.randomRIO (0, Vector.length Words.testWords - 1)
+    let w = Vector.unsafeIndex Words.testWords r
+    return (w, Morse.wordToMorse w)
 
 writePractice :: IO ()
 writePractice = do
