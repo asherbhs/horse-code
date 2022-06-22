d33884ce1357f6bd9a88892631f666cc3aeebccb
Asher Harvey-Smith <asher.harvey-smith@warwick.ac.uk>
2022-06-22 17:25:21 +0100

    add a ternary function and operator cos I saw it online and it looked cool

--- src/Tools.hs
+++ src/Tools.hs
@@ -1,4 +1,15 @@
-module Tools (padRight) where
+module Tools 
+    ( padRight
+    , iff
+    , (?)
+    ) where
+
+iff :: Bool -> a -> a -> a
+iff p x y = if p then x else y
+
+infixr 1 ?
+(?) :: Bool -> a -> a -> a
+(?) = iff
 
 padRight ::  Int -> a -> [a] -> [a]
 padRight n p xs
