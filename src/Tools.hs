31f71c1bbfe00798c3438a75c0864880f52080e1
Asher Harvey-Smith <asher.harvey-smith@warwick.ac.uk>
2022-06-22 17:07:11 +0100

    correct padRight function

--- src/Tools.hs
+++ src/Tools.hs
@@ -1,6 +1,6 @@
 module Tools (padRight) where
 
-padRight :: Integral a => a -> b -> [b] -> [b]
+padRight ::  Int -> b -> [b] -> [b]
 padRight n p xs
     | n <= 0 = xs
-    | otherwise = xs ++ replicate n p
+    | otherwise = xs ++ replicate (n - length xs) p
